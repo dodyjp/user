b02c7f9626c52a59eb4d7460c82787b2f3f3f861
xgrime <45916886+xgrime@users.noreply.github.com>
2018-12-21 09:42:16 +0700

    Update user.php

--- user.php
+++ user.php
@@ -8,9 +8,9 @@ $msgwe = "[!] info withdraw work with delay 10 sec";
 $msgveeu = "[!] enjoy your life";
 $data= [
  
-'asra 5115','Adhiel','Saiful Arifin','Qiandra','Wakhidul Kh S Kom','Hamka','Syahrul'       
+'asra 5115','Adhiel','Saiful Arifin','Qiandra','Wakhidul Kh S Kom','Hamka','Syahrul','Rohman Gado','Nur','Mince Mayola'       
  //contoh 'gratisan 2018','Gratisan 2018' besar kecilnya huruf sangat berpengaruh
  ];
- $toto = ['asra 5115','Adhiel','Saiful Arifin','Qiandra',];
+ $toto = ['asra 5115','Adhiel','Saiful Arifin','Qiandra','Wakhidul Kh S Kom','Hamka','Syahrul','Rohman Gado','Nur','Mince Mayola'];
  
 
