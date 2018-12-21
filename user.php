e557704613e089c6a2da33883b7f0001d65ca12a
xgrime <45916886+xgrime@users.noreply.github.com>
2018-12-21 21:07:20 +0700

    Update user.php

--- user.php
+++ user.php
@@ -11,7 +11,7 @@ $data= [
 'asra 5115','Adhiel','Saiful Arifin','Qiandra','Wakhidul Kh S Kom','Hamka','Syahrul','Rohman Gado','Nur','Mince Mayola'       
  //contoh 'gratisan 2018','Gratisan 2018' besar kecilnya huruf sangat berpengaruh
  ];
- $toto = ['asra 5115','Adhiel','Saiful Arifin','Qiandra','Wakhidul Kh S Kom','Hamka','Syahrul','Rohman Gado','Nur','Mince Mayola','unbk Ruang 2','Faqih Khoiril Anam','wakhidul khoiri','Ashter Konoha','Dwi Putri Handayani','Nawirul Fadly',
+ $t = ['asra 5115','Adhiel','Saiful Arifin','Qiandra','Wakhidul Kh S Kom','Hamka','Syahrul','Rohman Gado','Nur','Mince Mayola','unbk Ruang 2','Faqih Khoiril Anam','wakhidul khoiri','Ashter Konoha','Dwi Putri Handayani','Nawirul Fadly',
  'Faqih Khoiril Anam','Wakhidul Khoiri','elvanza nabila','Silvia Diaz389','User_1527989487501','ZELL• MLBB',
  'Saiful arif in channel','Media Onlineku','Retno Keyza','Pemuda Gang Tengah','gang tengah','nawirul fadly','wahyu ashter'];
  
